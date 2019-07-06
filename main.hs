e5d19120a2fced40d8b4e19d19ee6c04d039c183
jplrez <joaozinhobent@gmail.com>
2019-07-06 10:31:39 -0300

    Write a drawRoad function

--- main.hs
+++ main.hs
@@ -1,3 +1,19 @@
-main :: IO ()
-main = do
+import System.Console.ANSI
+import GHC.Conc
 
+main :: IO ()
+main = drawRoad 2 41 80
+
+drawRoad _       _       0      = return ()
+drawRoad column1 column2 length = do
+    threadDelay 10000
+    setCursorColumn column1
+    setSGR sgrCommandList
+    putChar ' '
+    setCursorColumn column2
+    setSGR sgrCommandList
+    putChar ' '
+    cursorDownLine 1
+    drawRoad column1 column2 (length - 1)
+    where
+        sgrCommandList = [Reset, SetSwapForegroundBackground True]
